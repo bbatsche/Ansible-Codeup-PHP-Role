e0a78595187fc67082aed550103acbe67460fd6f
Ben Batschelet <ben@codeup.com>
2016-03-22 19:14:47 -0500

    Basic set of specs

--- spec/bootstrap.rb
+++ spec/bootstrap.rb
@@ -13,6 +13,21 @@ set :disable_sudo, true
 
 # Set environment variables
 # set :env, :LANG => 'C', :LC_MESSAGES => 'C'
+set :shell, "/bin/bash"
+set :login_shell, true
 
 # Set PATH
 set :path, '/sbin:/usr/local/sbin:/usr/local/bin:$PATH'
+
+shared_examples "nginx::config" do
+  describe command("nginx -t") do
+    let(:disable_sudo) { false }
+
+    it "has no errors" do
+      expect(subject.stderr).to match /configuration file \/etc\/nginx\/nginx\.conf syntax is ok/
+      expect(subject.stderr).to match /configuration file \/etc\/nginx\/nginx\.conf test is successful/
+
+      expect(subject.exit_status).to eq 0
+    end
+  end
+end
